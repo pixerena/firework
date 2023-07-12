4d8b05f9a3a3c5c7fbea1dbf8ad5a2a9d5f322ee
趙子賢 <danny900714@gmail.com>
2023-07-13 03:59:02 +0800

    Add maven publish to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,15 @@
 plugins {
-    id("java-library")
+    `java-library`
+    `maven-publish`
 }
 
 group = "com.pixerena"
 version = "0.1.0-SNAPSHOT"
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
 
 repositories {
     mavenCentral()
@@ -21,6 +27,55 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            pom {
+                name.set("Firework")
+                description.set("The missing PaperMC plugin framework")
+                url.set("https://github.com/pixerena/firework")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("danny900714")
+                        name.set("Chao Tzu-Hsien")
+                        email.set("danny900714@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/pixerena/firework.git")
+                    developerConnection.set("scm:git:ssh://github.com/pixerena/firework.git")
+                    url.set("https://github.com/pixerena/firework")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            val isSnapshot = version.toString().endsWith("SNAPSHOT")
+            val releaseRepoUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotRepoUrl = uri("https://maven.pkg.github.com/pixerena/firework")
+
+            url = if (isSnapshot) snapshotRepoUrl else releaseRepoUrl
+            credentials {
+                username = System.getenv(if (isSnapshot) "GITHUB_ACTOR" else "MAVEN_USERNAME")
+                password = System.getenv(if (isSnapshot) "GITHUB_TOKEN" else "MAVEN_PASSWORD")
+            }
+        }
+    }
+}
+
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.javadoc {
+    if (JavaVersion.current().isJava9Compatible) {
+        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+    }
 }
